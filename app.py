7354484eac7aeadac6aae564b10c0a23089d9eba
MariaKlimatsida <m.klimatsida@yahoo.com>
2024-03-23 15:39:34 +0100

    Update app.py
    I update my hometown.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! My name is Maria Klimatsida.'
+    return 'Hello World! My name is Maria Klimatsida and I am from Thessaloniki.'
 
 
 if __name__ == "__main__":
